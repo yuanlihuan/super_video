68251af2c970dc6a9bc5f167ed80e93386d4c151
liulai <liulai@e-yuehui.com>
2019-03-27 10:18:38 +0800

    提交

--- super-video-dev/super-video-dev-mini-api/src/test/java/com/supers/demo/Test.java
+++ super-video-dev/super-video-dev-mini-api/src/test/java/com/supers/demo/Test.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class Test {
 	
 	public static void main(String[] args) {
-		List<Integer> strings = Arrays.asList(1,2,3,4,56,76,88,3,7);
-		strings.stream().sorted().forEach(System.out::println);
 	}
 
 
